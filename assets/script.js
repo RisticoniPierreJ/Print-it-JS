7b62f173dee92a1e8d3c24c7e6305e57b51f646c
Risticoni Pierre-Jean <rispj@yahoo.fr>
2023-12-03 17:07:47 +0100

    Mise en place du défilement à l'infinit

--- assets/script.js
+++ assets/script.js
@@ -16,60 +16,71 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-console.log(slides);
 
-// V1 Ajout des DOTS avec une classe suplémentaire number pour les différencier
+//********* Ajout des bullet points *********//
 let divAllDots = document.querySelector(".dots")
 
 for (let i = 0; i < slides.length; i++) {
 	divAllDots.innerHTML += `<div class="dot"></div>` 
 }
-console.log(divAllDots);
 
 
-//EventListener sur les flèches
-//Initialisation de la premiere image de la bannière
-i = 0
+//********* Initialisation de la bannière *********//
+let i = 0
 
+// Image et texte
 let imgBanner = document.querySelector(".banner-img")
 imgBanner.setAttribute("src", "./assets/images/slideshow/" + slides[i].image)
 
 let txtBanner = document.querySelector("#banner p")
 txtBanner.innerHTML += `${slides[i].tagLine}`	
 
+// bullet point
 let divDot = document.querySelectorAll(".dot")
 divDot[0].classList.add("dot_selected")
 
+//********* EventListener sur les flèches *********//
 
-let arrow_right = document.querySelector(".arrow_right")
-arrow_right.addEventListener("click", () => {
-
-	divDot[i].classList.remove("dot_selected")
-
-	i++
+// Fonction pour passé à l'image suivante
+function NextSlide(){
 	imgBanner.setAttribute("src", "./assets/images/slideshow/" + slides[i].image)
 	txtBanner.innerHTML = ``
-	txtBanner.innerHTML += `${slides[i].tagLine}`	
-
+	txtBanner.innerHTML += `${slides[i].tagLine}`
 	divDot[i].classList.add("dot_selected")
+}
 
+// Gestion du clic sur la flèche droite
+let arrow_right = document.querySelector(".arrow_right")
+arrow_right.addEventListener("click", () => {
+	
+	divDot[i].classList.remove("dot_selected")
+	
+	if(i === (slides.length-1)){
+		i=0
+		NextSlide()
+	}else{
+		i++
+		NextSlide()
+	}
 })
 
+// Gestion du clic sur la flèche gauche
 let arrow_left = document.querySelector(".arrow_left")
 arrow_left.addEventListener("click", () => {
 
 	divDot[i].classList.remove("dot_selected")
 
-	i--
-	imgBanner.setAttribute("src", "./assets/images/slideshow/" + slides[i].image)
-	txtBanner.innerHTML = ``
-	txtBanner.innerHTML += `${slides[i].tagLine}`	
-
-	divDot[i].classList.add("dot_selected")
-
-	
-
+	if(i === 0){
+		i=slides.length-1
+		NextSlide()
+	}else{
+		i--
+		NextSlide()
+	}
 })
+
+
+
 
 
 
